6a60369b96441ae0827138a174cd014c3c187cd7
c11060a <c11060a@W5140420>
2016-10-12 12:15:39 +0300

    Saved changes

---
+++ One/Test Solution/Print/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+
+namespace Print
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Printing odd numbers from 1 to 100");
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i % 2 == 1)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            Console.WriteLine("End");
+        }
+    }
+}
